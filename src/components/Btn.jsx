345df140c8b36182fd9c89759608d084016cee32
Abdul hanan butt <buttabdulhannan708@gmail.com>
2024-10-03 18:52:21 +0500

    bussiness section is complete

---
+++ src/components/Btn.jsx
@@ -0,0 +1,14 @@
+const Btn = () => {
+  return (
+    <div>
+      <button
+        type="button"
+        className={`ml-2 py-4 px-6 font-poppins font-medium text-primary outline-none bg-blue-gradient mt-10`}
+      >
+        Get Started
+      </button>
+    </div>
+  );
+};
+
+export default Btn;
