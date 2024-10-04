119c2c4337faee06b0f9a2e2f46fc877bc8768dd
Abdul hanan butt <buttabdulhannan708@gmail.com>
2024-10-04 12:08:24 +0500

    working on billing section

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import { bill } from "../assets";
+import { layout } from "../style";
 
 const Billing = () => {
-  return <div>Billing</div>;
+  return (
+    <>
+      <section id="product" className={layout?.sectionReverse}>
+        <div className={layout?.sectionImgReverse}>
+          <img src={bill} className="w-[100%] h-[100%] relative z-[5]" />
+
+          <div className="absloute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white_gradient" />
+          <div className="absloute z-[0] -left-1/2 bottom-0 w-[50%] h-[50%] rounded-full pink_gradient" />
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Billing;
